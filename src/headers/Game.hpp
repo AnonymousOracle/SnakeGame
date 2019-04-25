83e922911c26627374e37c6588fa0bca76817a96
Rostislav Tolushkin <Rostislav.Tolushkin@gmail.com>
2019-04-25 19:04:26 +0300

    Corrected vars due to code style

--- src/headers/Game.hpp
+++ src/headers/Game.hpp
@@ -15,11 +15,12 @@ class Game
     };
     RenderData get_render_data();
     bool is_game_over();
+    void set_cell_state();
 
     private:
     static const int WIDTH = 20;
     static const int HEIGHT = 20;
-    int fieldState[WIDTH][HEIGHT];
+    int field_state[WIDTH][HEIGHT];
 };
 
 
--- src/sources/Game.cpp
+++ src/sources/Game.cpp
@@ -8,7 +8,7 @@ RenderData Game::get_render_data()
     rendered_data.width = WIDTH;
     rendered_data.height = HEIGHT;
     
-    matrix_copy(fieldState, rendered_data.field, 20);
+    matrix_copy(field_state, rendered_data.field, 20);
 
     return rendered_data;
 }
